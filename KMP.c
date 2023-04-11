b4f428b30fc33e5881af44df299e632b51a01bc2
Dhruvin Chawda <114014218+Dhruvin3103@users.noreply.github.com>
2023-04-11 11:20:45 +0530

    Create KMP.c

---
+++ KMP.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<string.h>
+
+int *prefix_fun(char *p)
+{
+    static int pie[100],k=0;
+    pie[0]=0;
+    // printf("hi");
+    for(int i=1; i<strlen(p);i++)
+    {
+        // printf("\nk %d",k);
+        while(k>0 && p[k]!=p[i])
+        {
+            k=pie[k];
+        }
+        // printf("|%c %c- %d %d|",p[k],p[i],k,i);
+        if(p[k]==p[i])
+            {// {printf("|%c %c|",p[k],p[i]);
+            k+=1;}
+        pie[i]=k;
+    }
+    return pie;
+}
+
+// int *retarr(int *p)
+// {
+//     printf("%c",p[0]);
+//     p[0]=p[0]+"h";
+//     printf("%c",p[0]);
+//     return p;
+// }
+
+void main()
+{
+    int tlen,plen,q=0;
+    int *pie;
+    char text[200]="ababcabcabababd",pat[100]="ababd";
+    tlen = strlen(text);
+    plen = strlen(pat);
+    pie = prefix_fun(pat);
+    // printf("\nLength : %d %d\n",tlen,plen);
+    for(int i=0;i<plen;i++)
+        printf("pie elements at 0 pos: %d\n", pie[i]);
+    for(int i=0;i<tlen;i++)
+    {
+        // printf("|%d|",q);
+        while(q>0 && pat[q]!=text[i])
+        {
+            
+            q=pie[q-1];
+            // printf("\npie %c %c %d\n",pat[q],text[i],q);
+        }
+        if(pat[q]==text[i])
+        {
+            // printf("\n+ %c %c %d\n",pat[q],text[i],q);
+            q+=1;
+        }
+        if(q==plen)
+        {
+            printf("pattern occurs at %d",i-plen+1);
+            printf("\n");
+            for(int j=(i-plen+1);j<i+1;j++)
+            {printf("%c",text[j]);}
+            q=pie[q-1];
+        }
+    }
+
+}
