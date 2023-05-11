a8abd0fb80d7314ba5bba0abf151a282e736a75b
Dhruvin Chawda <114014218+Dhruvin3103@users.noreply.github.com>
2023-05-11 19:54:07 +0530

    Update rabinkarp2.c

--- rabinkarp2.c
+++ rabinkarp2.c
@@ -1,59 +1,24 @@
-#include <stdio.h>
-#include <string.h>
-#define d 256
-void search(char pat[], char txt[], int q)
-{
-    int M = strlen(pat);
-    int N = strlen(txt);
-    int i, j;
-    int p = 0; // hash value for pattern
-    int t = 0; // hash value for txt
-    int h = 1;
-    // The value of h would be "pow(d, M-1)%q"
-    for (i = 0; i < M - 1; i++)
-        h = (h * d) % q;
-    // Calculate the hash value of pattern and first
-    // window of text
-    for (i = 0; i < M; i++)
-    {
-        p = (d * p + pat[i]) % q;
-        t = (d * t + txt[i]) % q;
-    }
-    // Slide the pattern over text one by one
-    for (i = 0; i <= N - M; i++)
-    {
-        // Check the hash values of current window of text
-        // and pattern. If the hash values match then only
-        // check for characters one by one
-        if (p == t)
-        {
-            /* Check for characters one by one */
-            for (j = 0; j < M; j++)
-            {
-                if (txt[i + j] != pat[j])
-                    break;
-            }
-            // if p == t and pat[0...M-1] = txt[i, i+1, ...i+M-1]
-            if (j == M)
-                printf("Pattern found at index %d \n", i);
-        }
-        // Calculate hash value for next window of text: Remove
-        // leading digit, add trailing digit
-        if (i < N - M)
-        {
-            t = (d * (t - txt[i] * h) + txt[i + M]) % q;
-            // We might get negative value of t, converting it
-            // to positive
-            if (t < 0)
-                t = (t + q);
-        }
-    }
-}
-int main()
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+void main()
 {
     char txt[] = "xyzahxyfxyzd";
     char pat[] = "xyz";
-    int q = 101;
-    search(pat, txt, q);
-    return 0;
+    int pathash=0,i,j,k,txthash,flag=0;
+    for(i=0;i<strlen(pat);i++)
+    pathash+=pat[i]*pow(10,strlen(pat)-i-1);
+    for(i=0;i<strlen(txt);i++)
+    {
+        txthash=0;
+        for(j=i,k=0;j<strlen(pat)+i;j++,k++)
+        txthash+=txt[j]*pow(10,strlen(pat)-k-1);
+        if(txthash==pathash)
+        {
+            printf("String Matched at index %d\n",i);
+            flag=1;
+        }
+    }
+    if(flag==0)
+    printf("String not matched");
 }
